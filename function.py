0d74a1489c9a95cc97d48e3290c0aee2c4fce28d
harsh24091975 <74312669+harsh24091975@users.noreply.github.com>
2021-10-19 15:11:14 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,14 @@
+def swapfiledata():
+    file1=input("enter 1 file = ")
+    file2=input("enter 2 file = ")
+
+    with open(file1,'r') as a:
+        data_a = a.read()
+    with open(file2,'r') as b:
+        data_b = b.read()
+    with open(file1,'w') as a:
+        a.write(data_b)
+    with open(file2,'w') as b:
+        b.write(data_a)
+
+swapfiledata()
